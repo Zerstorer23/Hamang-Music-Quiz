2ffc13cbb77ea3232a25047a905b044ed7c854f5
Zerstorer23 <bismark439@gmail.com>
2022-06-22 19:03:30 -0500

    AAUto Insert answer

--- fbkey/fakeKey.tsx
+++ fbkey/fakeKey.tsx
@@ -1,5 +1,9 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+/*
+* 이 파일에 파이어페이스 ㅈ정보 입력후
+* SGconfig.tsx로 이름변경후
+* src/Database/keys폴더에 넣을것
+* */
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const FIRE_API_KEY = "Some Firebase API Key";
